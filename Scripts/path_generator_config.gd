bb9bb9f36659c4bbfd94520c01f304e30e5b0cbc
vikketik <124299223+vikketik@users.noreply.github.com>
2024-11-02 21:02:28 +0100

    test

--- Scripts/path_generator_config.gd
+++ Scripts/path_generator_config.gd
@@ -1,8 +1,8 @@
 extends Resource
 class_name PathGeneratorConfig
 
-@export var map_height : int = 9
-@export var map_lenght : int = 16
+@export var map_length:int = 16
+@export var map_height:int = 9
 
 @export var min_path_size = 50
 @export var max_path_size = 70
